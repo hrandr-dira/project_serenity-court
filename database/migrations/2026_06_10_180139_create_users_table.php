c72c2062abad9248458b05a86485591fa15d88a6
hrandr-dira <heraandira123@gmail.com>
2026-06-11 02:26:07 +0800

    membuat tabel users

---
+++ database/migrations/2026_06_10_180139_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Schema::create('users', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->timestamps();
+        // });
+
+        Schema::create('users', function (Blueprint $table) {
+            $table->string('id_user', 36)->primary();
+            $table->string('nama_lengkap', 50);
+            $table->string('email')->unique();
+            $table->string('no_telepon', 15);
+            $table->enum('peran', ['Admin', 'Customer']);
+            $table->date('tanggal_daftar');
+            $table->enum('status_akun', ['Aktif', 'Tidak Aktif']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
